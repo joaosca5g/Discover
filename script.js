dd743eb940179ca3b9d449af69d226d1508b0885
joaosca5g <joaopedroscasantos@gmail.com>
2024-07-29 22:19:42 -0300

    New version of project

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function toggleMode(){
 
     //substituir a imagem
     if(html.classList.contains('light')){ // se tiver light mode, adicionar a imagem light
-        img.setAttribute("src","./assets/avatar-light.png")
+        img.setAttribute("src","./assets/avatar_two.png")
     }else{
         //se tiver sem light mode, manter a imagem normal.
     
-        img.setAttribute("src", "./assets/avatar.png")
+        img.setAttribute("src", "./assets/avatar_happy.png")
     }
 
     
